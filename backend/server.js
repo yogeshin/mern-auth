59e5dba85abccc816bc4f93626994ca97adf04b5
Yogesh Shinde <yogesh.krishna.shinde@ing.com>
2023-06-09 20:54:11 +0530

    Add server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,9 +20,18 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) =>
-  res.send(`Server is ready on ${process.env.NODE_ENV} environment`)
-);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
